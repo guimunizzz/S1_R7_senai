037373d16a209d4c8e8a65a381d55219c33a17b4
guimunizzz <guilherme.muniz5@hotmail.com>
2025-04-02 15:46:51 -0300

    corrigindo formatação de valores

--- Exercicio2/index.js
+++ Exercicio2/index.js
@@ -11,25 +11,25 @@ if (genero != "M" && genero != "H") {
   } else {
     if (genero == "M") {
       if (qntdAnos > 20) {
-        novoSalario = salarioAtual + salarioAtual * 0.23;
+        novoSalario = salarioAtual + (salarioAtual * 0.23);
       } else if (qntdAnos >= 15 && qntdAnos <= 20) {
-        novoSalario = salarioAtual + salarioAtual * 0.12;
+        novoSalario = salarioAtual + (salarioAtual * 0.12);
       } else if (qntdAnos >= 5 && qntdAnos <= 10) {
-        novoSalario = salarioAtual + salarioAtual * 0.07;
+        novoSalario = salarioAtual + (salarioAtual * 0.07);
       } else if (qntdAnos < 5) {
-        novoSalario = salarioAtual + salarioAtual * 0.04;
+        novoSalario = salarioAtual + (salarioAtual * 0.04);
       } else {
         alert("Voce inseriu uma data invalida")
       }
     } else {
         if (qntdAnos > 30) {
-            novoSalario = salarioAtual + salarioAtual * 0.25;
+            novoSalario = salarioAtual + (salarioAtual * 0.25);
           } else if (qntdAnos >= 20 && qntdAnos <= 30) {
-            novoSalario = salarioAtual + salarioAtual * 0.14;
+            novoSalario = salarioAtual + (salarioAtual * 0.14);
           } else if (qntdAnos >= 5 && qntdAnos <= 15) {
-            novoSalario = salarioAtual + salarioAtual * 0.08;
+            novoSalario = salarioAtual + (salarioAtual * 0.08);
           } else if (qntdAnos < 5) {
-            novoSalario = salarioAtual + salarioAtual * 0.03;
+            novoSalario = salarioAtual + (salarioAtual * 0.03);
           } else {
             alert("Voce inseriu uma data invalida")
           }
