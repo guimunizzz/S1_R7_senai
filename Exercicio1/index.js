c4ed74c0bea6bba2b3447d1f5afdeae81bbbb282
guimunizzz <guilherme.muniz5@hotmail.com>
2025-04-02 13:06:55 -0300

    Término do exercicio do aluguel de carro

---
+++ Exercicio1/index.js
@@ -0,0 +1,29 @@
+let tipoDoCarro = prompt(
+  "Qual o tipo do carro alugado? (DIGITE 'popular' ou 'luxo'");
+
+if (tipoDoCarro != "popular" && "luxo") {
+  alert("Voce digitou o tipo de carro de forma errada!");
+} else {
+  let diasAlugados = parseInt(prompt("Quantos dias de aluguel?"));
+  let kmPercorridos = parseFloat(prompt("Quantos KM foram percorridos?"));
+  let valorTotal = parseFloat;
+
+  if (isNaN(diasAlugados && kmPercorridos)) {
+    alert("ERRO - digite um numero!");
+  } else {
+    if (tipoDoCarro == "popular") {
+      if (kmPercorridos > 100) {
+        valorTotal = diasAlugados * 90 + 0.1 * kmPercorridos;
+      } else {
+        valorTotal = diasAlugados * 90 + 0.2 * kmPercorridos;
+      }
+    } else {
+      if (kmPercorridos > 200) {
+        valorTotal = diasAlugados * 150 + 0.25 * kmPercorridos;
+      } else {
+        valorTotal = diasAlugados * 150 + 0.3 * kmPercorridos;
+      }
+    }
+    alert(`O valor total a pagar é de: ${valorTotal}`);
+  }
+}
