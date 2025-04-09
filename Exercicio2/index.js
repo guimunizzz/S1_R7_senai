5b5682a9dbf6432744ea0300034095e2489920ca
guimunizzz <guilherme.muniz5@hotmail.com>
2025-04-09 07:52:07 -0300

    Formatação de texto e alteração de verificação

--- Exercicio2/index.js
+++ Exercicio2/index.js
@@ -1,12 +1,12 @@
-let genero = prompt("Digite o genero do funcionario (DIGITE 'M' ou 'H'");
+let genero = prompt("Digite o genero do funcionario (DIGITE 'M' ou 'H')");
 
-if (genero != "M" && genero != "H") {
+if (genero != "M" && genero != "H") { //Verificação se digitou o genero da forma correta
   alert("ERRO - Você digitou de forma errada!!");
 } else {
   let salarioAtual = parseFloat(prompt("Digite o salário atual do funcionario: "));
   let qntdAnos = parseInt(prompt("Digite quanto tempo o funcionario trabalha na empresa: "));
   let novoSalario;
-  if (isNaN(salarioAtual && qntdAnos)) {
+  if (isNaN(salarioAtual && qntdAnos)) { // Verificação se foi digitado um numero nas variaveis salario e anos
     alert("ERRO - digite um numero!");
   } else {
     if (genero == "M") {
@@ -14,7 +14,7 @@ if (genero != "M" && genero != "H") {
         novoSalario = salarioAtual + (salarioAtual * 0.23);
       } else if (qntdAnos >= 15 && qntdAnos <= 20) {
         novoSalario = salarioAtual + (salarioAtual * 0.12);
-      } else if (qntdAnos >= 5 && qntdAnos < 15) {
+      } else if (qntdAnos >= 5 && qntdAnos <= 10) {
         novoSalario = salarioAtual + (salarioAtual * 0.07);
       } else if (qntdAnos < 5) {
         novoSalario = salarioAtual + (salarioAtual * 0.04);
@@ -26,7 +26,7 @@ if (genero != "M" && genero != "H") {
             novoSalario = salarioAtual + (salarioAtual * 0.25);
           } else if (qntdAnos >= 20 && qntdAnos <= 30) {
             novoSalario = salarioAtual + (salarioAtual * 0.14);
-          } else if (qntdAnos >= 5 && qntdAnos < 20) {
+          } else if (qntdAnos >= 5 && qntdAnos <= 15) {
             novoSalario = salarioAtual + (salarioAtual * 0.08);
           } else if (qntdAnos < 5) {
             novoSalario = salarioAtual + (salarioAtual * 0.03);
